ef79cd6f78c68f515d334096281f9d009d63b26b
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2025-08-24 15:28:51 -0300

    feat: styles rewards

--- src/components/Rewards/index.tsx
+++ src/components/Rewards/index.tsx
@@ -1,3 +1,7 @@
 export default function Rewards() {
-  return <div className="border w-full h-[114px]"></div>;
+  return <div className="border w-full h-[114px] bg-[#212430] flex items-center justify-center">
+    <div className="w-[140px] h-[64px] border"></div>
+    <div className="w-[140px] h-[64px] border"></div>
+    <div className="w-[140px] h-[64px] border"></div>
+  </div>;
 }
