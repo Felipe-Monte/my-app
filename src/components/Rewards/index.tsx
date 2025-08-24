f1318d87045f1a4450067d92acf64a5adaf43901
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2025-08-24 16:26:09 -0300

    feat: add rewards component

--- src/components/Rewards/index.tsx
+++ src/components/Rewards/index.tsx
@@ -1,7 +1,33 @@
+import { IconDol, IconTrophy, IconUser } from '../../../public/assets/svg';
+
 export default function Rewards() {
-  return <div className="border w-full h-[114px] bg-[#212430] flex items-center justify-center">
-    <div className="w-[140px] h-[64px] border"></div>
-    <div className="w-[140px] h-[64px] border"></div>
-    <div className="w-[140px] h-[64px] border"></div>
-  </div>;
+  return (
+    <div className=" w-full h-[114px] bg-[#212430] flex items-center justify-around">
+      <div className="flex flex-col items-center justify-center min-w-[100px]">
+        <span>
+          <IconTrophy />
+        </span>
+        <span className="font-bold text-4xl">124</span>
+        <span className="font-medium text-xs text-[#747DA4]">Campeonatos</span>
+      </div>
+
+      <div className="flex flex-col items-center justify-center min-w-[100px]">
+        <span>
+          <IconUser />
+        </span>
+        <span className="font-bold text-4xl">2.6M</span>
+        <span className="font-medium text-xs text-[#747DA4]">Seguidores</span>
+      </div>
+
+      <div className="flex flex-col items-center justify-center min-w-[100px]">
+        <span>
+          <IconDol />
+        </span>
+        <span className="font-bold text-4xl">14M</span>
+        <span className="font-medium text-xs text-[#747DA4]">
+          Recompensados
+        </span>
+      </div>
+    </div>
+  );
 }
