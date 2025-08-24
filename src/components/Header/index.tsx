d686b1c00819599827c63791cf420fd31019335c
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2025-08-24 12:56:02 -0300

    feat: init training repo

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import IconMenu, { Logo } from '../../../public/assets/svg';
+
+export default function Header() {
+  return (
+    <div className="w-full h-[71px] bg-[#212430] flex items-center justify-between px-4 py-5">
+      <div>
+        <Logo />
+      </div>
+
+      <div className="flex gap-3.5 items-center justify-center">
+        <div className="bg-[#8FFF00] text-[#203800] font-bold text-base py-1.5 px-3.5 rounded-[12px]">
+          Donate
+        </div>
+        <div>
+          <IconMenu />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SocialArea/index.tsx
@@ -0,0 +1,13 @@
+import { IconDiscord, IconTwitch, IconWolf } from '../../../public/assets/svg';
+
+export default function SocialArea() {
+  return (
+    <div className="h-[45px] w-full px-4 bg-[#212430]">
+      <div className=" flex gap-2.5 w-full h-full justify-end">
+        <IconWolf />
+        <IconTwitch />
+        <IconDiscord />
+      </div>
+    </div>
+  );
+}
