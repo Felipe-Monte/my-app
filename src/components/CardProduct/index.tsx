ed21aaffd72f41a45458028a629c562f09261fc9
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2025-08-24 22:46:18 -0300

    feat: working in gear

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,3 +1,27 @@
+import Image from 'next/image';
+
 export default function CardProduct() {
-  return <div className="w-full h-[140px] rounded-xl border"></div>;
+  return (
+    <div className="w-full h-[140px] rounded-xl bg-[#2F3651] flex">
+      <div className="w-[130px] h-full relative">
+        <Image
+          src="/assets/img/mouse-razer.png"
+          fill
+          className="object-cover"
+          alt="mouse image"
+        />
+      </div>
+
+      <div className='border flex-1 px-5 items-center justify-center'>
+
+        <div className='border'>
+          <p>Mouse</p>
+          <p>Razer Deathadder R-X1</p>
+          <p>View More</p>
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
