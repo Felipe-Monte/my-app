e1fe36e2918aea6e7c758e33b46f581cd2e1ab43
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2025-08-30 18:45:50 -0300

    feat: mock items

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,12 +1,20 @@
 import Image from 'next/image';
 import { IconArrowRight } from '../../../public/assets/svg';
 
-export default function CardProduct() {
+
+type CardProductType = {
+  image: string;
+  mark: string;
+  item: string;
+  name: string;
+}
+
+export default function CardProduct({ image, mark, item, name } : CardProductType) {
   return (
     <div className="w-full h-[140px] rounded-xl bg-[#2F3651] flex">
       <div className="w-[130px] h-full relative">
         <Image
-          src="/assets/img/mouse-razer.png"
+          src={image}
           fill
           className="object-cover"
           alt="mouse image"
@@ -15,10 +23,10 @@ export default function CardProduct() {
 
       <div className="flex-1 px-5 items-center justify-center rounded-r-xl">
         <div className="flex flex-col gap-2.5 items-start justify-center h-full">
-          <p className="font-bold text-[10px] text-[#747DA4]">Mouse</p>
+          <p className="font-bold text-[10px] text-[#747DA4]">{item}</p>
           <p className="font-extrabold text-[12px] text-[#0fff00]">
-            Razer{' '}
-            <span className="font-medium text-white">Deathadder R-X1</span>
+            {mark}{' '}
+            <span className="font-medium text-white">{name}</span>
           </p>
           <div className="flex items-center justify-center gap-1">
             <p className="font-bold text-[10px] text-[#747DA4]">View More </p>
--- src/components/CardsGear/index.tsx
+++ src/components/CardsGear/index.tsx
@@ -1,12 +1,41 @@
 import CardProduct from '../CardProduct';
 
+const products = [
+  {
+    image: '/assets/img/mouse-razer.png',
+    item: 'Mouse',
+    mark: 'Razer',
+    name: 'Deathadder R-X1',
+  },
+  {
+    image: '/assets/img/mouse-razer.png',
+    item: 'Mouse',
+    mark: 'Razer',
+    name: 'Deathadder R-X1',
+  },
+  {
+    image: '/assets/img/mouse-razer.png',
+    item: 'Mouse',
+    mark: 'Razer',
+    name: 'Deathadder R-X1',
+  },
+];
+
 export default function CardsGear() {
   return (
     <div className="w-full h-[486px]">
       <h2 className="font-bold text-xl mb-2.5">MEU GEAR</h2>
 
-      <main>
-        <CardProduct />
+      <main className="flex flex-col gap-3.5">
+        {products.map((item, index) => (
+          <CardProduct
+            key={index}
+            image={item.image}
+            item={item.item}
+            mark={item.mark}
+            name={item.name}
+          />
+        ))}
       </main>
     </div>
   );
